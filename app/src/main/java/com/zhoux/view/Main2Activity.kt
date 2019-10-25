eeff26169035e630b5a2d7684ac82ff38a03052e
周祥 <601635680@qq.com>
2019-10-25 10:18:31 +0800

    首次提交

---
+++ app/src/main/java/com/zhoux/view/Main2Activity.kt
@@ -0,0 +1,20 @@
+package com.zhoux.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+        CircularProgressView.isStartDraw = true
+        CircularProgressView.progress=20
+        CircularProgressView.invalidate()
+
+
+    }
+}
---
+++ app/src/main/java/com/zhoux/view/Utils.java
@@ -0,0 +1,22 @@
+package com.zhoux.view;
+
+import android.content.Context;
+
+public class Utils {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+    /**
+     * convert sp to its equivalent px
+     * <p>
+     * 将sp转换为px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
